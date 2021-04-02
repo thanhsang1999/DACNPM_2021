f5ba9be357a2723a2bd6ae0881569b64e0b7ffc4
thanhsang1999 <nguyenhuythanhsang@gmail.com>
2021-04-02 13:47:55 +0700

    first pust

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./index.css";
+App.propTypes = {};
+
+function App(props) {
+  return <div></div>;
+}
+
+export default App;
